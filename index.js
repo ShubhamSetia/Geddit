b8ed3fb95eac97447a7f84ff0f1c2db0e4fec44c
ShubhamSetia <shubham.setia12@gmail.com>
2017-10-02 18:50:00 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database')
+const path = require('path')
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.uri, {useMongoClient: true,},(err) => {
+  //console.log("in here");
+  if (err){
+    console.log('Could not connect to database: ',err)
+  }
+  else{
+    console.log(config.secret)
+    console.log('Connected to database: '+config.db)
+  }
+});//
+//console.log(config.secret)
+
+app.use(express.static(__dirname+ '/client/dist/'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+});
+
+app.listen(8080,() => {
+  console.log('Listening on port 8080 ');
+});
